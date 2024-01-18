e3bf7d1c4d5a439e1f0aa65d9f4710cb732994cd
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2024-01-19 00:24:32 +0530

    landing page done

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,36 @@
-
-export default function Home() {
+import Link from "next/link";
+import landing from '../assets/images/landing.svg'
+import Image from "next/image";
+const Landing = () => {
   return (
-    <main className="">
-      Home Page
-    </main>
-  )
-}
+    <>
+      <main className=" h-[100vh]  p-6 flex flex-col md:ml-10 ">
+        <div className=" my-4 ">
+          LOGO
+        </div>
+        <div className=" mt-44  md:mt-24 flex flex-col items-center md:flex-row md:justify-evenly md:items-start ">
+          <div className=" md:w-2/6 md:mt-20">
+            <p className=" text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              odio laudantium eos a, cupiditate repellat nam velit suscipit eius
+              facere quidem nulla veritatis magni iusto numquam aliquid
+              accusantium aut veniam atque. Dolore, sed quo! Expedita
+              perferendis
+            </p>
+            <Link href="/login">
+              <button className="border text-sm border-[--primary-dark-blue1] px-2 py-1 b  rounded-md bg-white text-[--primary-dark-blue1]  mt-10 hover:text-white hover:bg-[--primary-dark-blue1] transition-all duration-200 ease-in ">
+                {/* Login / Register */}
+                Proceed to Login
+              </button>
+            </Link>
+          </div>
+          <div className="">
+            <Image src={landing} alt="" className="hidden md:block h-[25rem]" />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Landing
